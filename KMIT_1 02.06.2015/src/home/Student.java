f030ffb439fffa969e0c41a7caf707ef48258552
monarch086 <sbarsuk88@gmail.com>
2015-06-06 00:19:47 +0300

    Commit

---
+++ KMIT_1 02.06.2015/src/home/Student.java
@@ -0,0 +1,28 @@
+package home;
+
+public class Student {
+
+	private String name;
+	private int subject_java;
+	private int subject_math;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSubject_java() {
+		return subject_java;
+	}
+	public void setSubject_java(int subject_java) {
+		this.subject_java = subject_java;
+	}
+	public int getSubject_math() {
+		return subject_math;
+	}
+	public void setSubject_math(int subject_math) {
+		this.subject_math = subject_math;
+	}
+	
+}
